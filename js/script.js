0e32bc1c321dd311dc1147f72ba9e9fe7b118f19
Marquinhus <marcuszhrt@gmail.com>
2022-07-25 22:15:48 -0300

    atualização js

---
+++ js/script.js
@@ -0,0 +1,50 @@
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link){
+const url = location.href;
+const href = link.href;
+
+if(url.includes(href)) {
+ link.classList.add("ativo")
+}
+}
+
+links.forEach(ativarLink); 
+
+//parametros linkados//
+
+const parametros = new URLSearchParams (location.search);
+
+function ativarCheck(parametro){
+const elemento = document.getElementById(parametro);
+if(elemento)
+{elemento.checked = true;}
+}
+
+parametros.forEach(ativarCheck);
+
+
+//perguntas frequentes//
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarpergunta(event){
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+ const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+  
+
+
+}
+
+function eventosPerguntas(pergunta){
+  pergunta.addEventListener("click",ativarpergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+//galeria de bicicletas//
